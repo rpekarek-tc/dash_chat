868583a0fb156fd7959d6c0a8fdf983d5f37c7ec
rpekarek <rpekarek@gloo.us>
2021-04-09 12:13:41 -0600

    Revert "requesting focus on mention tap"
    This reverts commit dc2f1d9d33beff6b2a2b8a74384e6a90fb83bbf7.

--- lib/src/chat_input_toolbar.dart
+++ lib/src/chat_input_toolbar.dart
@@ -101,7 +101,6 @@ class AutoCompleteChatInputToolbar extends StatelessWidget {
                       hideOnEmpty: true,
                       hideSuggestionsOnKeyboardHide: true,
                       onSuggestionSelected: (suggestion) {
-                        focusNode.requestFocus();
                         int cursor = controller.value.selection.base.offset;
                         int lastAtSymbol = controller.text
                             .substring(0, cursor)
